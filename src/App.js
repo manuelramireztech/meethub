36d5085c8d18db60a843d139390064159680d3ea
Full Stack <7883863+FullStackForce@users.noreply.github.com>
2020-06-10 18:52:18 -0400

    add it more feature font-end and routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import Video from './Video'
 import Home from './Home'
+import Registration from './registrationForm'
+import Login from './loginForm'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 class App extends Component {
@@ -9,7 +11,9 @@ class App extends Component {
 			<div>
 				<Router>
 					<Switch>
-						<Route path="/" exact component={Home} />
+						<Route path="/" exact component={Login} />
+						<Route path="/Registration" component={Registration} />
+						<Route path="/Home" component={Home} />
 						<Route path="/:url" component={Video} />
 					</Switch>
 				</Router>
